414f9da90188bf2c512b8edf3bf7ee2019600587
Neo-Uli <diegouliseslpz36@gmail.com>
2023-11-20 22:13:09 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+//Importando el componente Card
+//import Card from "./components/Card"
+//Usando el componente Cards
+import Cards from "./components/Cards";
+
+const App = () => {
+  return <Cards />
+};
+
+export default App
